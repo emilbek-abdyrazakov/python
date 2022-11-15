1acd561301ed3e5bde57b702db89ea2cec5a7493
Emil A <moorkgz@gmial.com>
2022-11-14 18:03:04 -0600

    new

--- sum13_2.py
+++ sum13_2.py
@@ -8,3 +8,8 @@ def sum13_2(nums):
 
 print(sum13_2([1, 2, 2, 1]))
 print(sum13_2([1, 2, 2, 1, 13]))
+print(sum13_2([13, 0, 13]))
+print(sum13_2([5, 13, 2]))
+print(sum13_2([13, 1, 2, 13, 2, 1, 13]))
+print(sum13_2([13]))
+print(sum13_2([]))
